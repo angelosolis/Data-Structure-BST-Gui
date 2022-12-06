d86d3d39ab07e6a2884856a76a47e8b8e5dbe7aa
Angelo Solis <88774848+angelosolis@users.noreply.github.com>
2022-12-06 20:44:05 +0800

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,34 @@
+
+//Sets the principal tools we need to construct the tree
+public class Node{
+    //constructor
+    int content;
+    Node left;
+    Node right;
+    
+    //This method put data in the Node of the tree
+    public Node(int c) {
+		this.content = c;
+	}
+    
+    //This method gets the data of a Node in the tree
+    public int getContent() { return content; }
+    
+    //This method gets the next node located in the left of another node
+    public Node getLeft() { return left; }
+    /*In the binary tree sets the node to the left, only if it's less than other node.
+    it also receives an int, int that will be stored in the node seted*/
+    public Node setLeft(int content) {
+	left = new Node(content);
+	return left;
+    }
+    
+    //This method gets the next node located in the right of another node
+    public Node getRight() { return right; }
+    /*In the binary tree sets the node to the right, only if it's larger than other node.
+    it also receives an int, int that will be stored in the node seted*/
+    public Node setRight(int content) {
+	right = new Node(content);
+	return right;
+    }   
+}
